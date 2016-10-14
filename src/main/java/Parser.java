9297afe51efa5b158af9753b997dd8d51d6ce8fa
aloysius <aloy-wee-sious@hotmail.com>
2016-10-13 17:50:17 -0700

    added basic structure for parser

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -11,7 +11,26 @@ public class Parser {
         COMMAND_ADDME
     }
 
+    public enum adminCommands{
+        ADMIN_VIEW_USERS,
+        ADMIN_ADD,
+        ADMIN_REMOVE_USERS,
+        ADMIN_VIEW_PENDING,
+        ADMIN_REMOVE_PENDING,
+        ADMIN_VIEW_REQUEST,
+        ADMIN_VIEW_REMIND,
+        ADMIN_PUBLISH,
+        ADMIN_HELP
+    }
 
+    public static Parser.adminCommands parseAdminCommand(String command){
+        //split string to tokens and compare first token
+        //return them as commands
+        return adminCommands.ADMIN_ADD;
+    }
 
+    public static Parser.userCommands parseUserCommand(String command){
 
+        return userCommands.COMMAND_ADD;
+    }
 }
