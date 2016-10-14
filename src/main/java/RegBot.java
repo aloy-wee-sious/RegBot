29982231c31a39d893f25988d6a56ad153f8e162
aloy-wee-sious <aloy-wee-sious@hotmail.com>
2016-10-13 23:53:11 -0700

    implemented parser class

--- src/main/java/RegBot.java
+++ src/main/java/RegBot.java
@@ -17,30 +17,9 @@ public class RegBot extends TelegramLongPollingBot {
         this.config = new BotConfig(input);
     }
 
-    private final String COMMAND_ADD = "/add";
-    private final String COMMAND_VIEW = "/view";
-    private final String COMMAND_DELETE = "/delete";
-    private final String COMMAND_HELP = "/help";
-    private final String COMMAND_ADDME = "/addme";
-    private final String COMMAND_START = "/start";
 
     private ArrayList<User> seekApproval = new ArrayList<>(Arrays.asList(new User("wait", "longlong", (long)1111111111)));
     private ArrayList<User> myUsers = new ArrayList<User>(Arrays.asList(new User("Aloysius", "Wang", (long)226481140), new User("fakeguy", null, (long)000000000)));
-
-    private final String ADMIN_VIEW_USERS = "/adminviewusers";
-    private final String ADMIN_ADD = "/adminadd";
-    private final String ADMIN_REMOVE_USERS = "/adminremoveuser";
-
-    private final String ADMIN_VIEW_PENDING = "/adminviewpending";
-    private final String ADMIN_REMOVE_PENDING = "/adminremovepending";
-
-    private final String ADMIN_VIEW_REQUEST = "/adminviewrequest";
-    private final String ADMIN_VIEW_REMIND = "/adminremind";
-    private final String ADMIN_PUBLISH = "/adminpublish";
-
-    private final String ADMIN_NEWADMIN = "/adminnew";
-    private final String ADMIN_HELP = "/adminhelp";
-
 
 
     public void onUpdateReceived(Update update) {
@@ -52,18 +31,21 @@ public class RegBot extends TelegramLongPollingBot {
                 if (message.isUserMessage()) {
                     System.out.println("user pm with text " + message.getText());
                     String text = formatText(message.getText());
-                    String command = getCommand(text);
-                    text = removeCommand(text);
                     String reply = "";
+                    if(Parser.isAdminCommand(text) && AdminCommands.isAdmin(message.getFrom().getId())){
+                        Parser.adminCommands adminCommand = Parser.parseAdminCommand(text);
+                        text = removeCommand(text);
+                        reply = processAdminCommands(adminCommand, text);
+                        System.out.println(adminCommand);
+                    }else{
+                        Parser.userCommands userCommand = Parser.parseUserCommand(text);
+                        text = removeCommand(text);
+                        reply = processUserCommands(userCommand, new User( message.getFrom().getFirstName(), message.getFrom().getLastName(), message.getFrom().getId()), text);
+                        System.out.println(userCommand);
+                    }
                     boolean isAdmin = AdminCommands.isAdmin(message.getFrom().getId());
                     System.out.println("is admin command " + isAdminCommand(formatText(message.getText())));
                     System.out.println("is admin " + isAdmin+"");
-                    System.out.println(command);
-                    if(isAdminCommand(message.getText()) && isAdmin){
-                        reply = processAdminCommands(command, text);
-                    }else{
-                        reply = processUserCommands(command, new User( message.getFrom().getFirstName(), message.getFrom().getLastName(), message.getFrom().getId()), text);
-                    }
                     sendReply(message, sendMessage, reply);
                 } else if (message.isGroupMessage() && message.getText().contains("@ReggyBot")) {
                     String reply = formatText(message.getText());
@@ -80,7 +62,7 @@ public class RegBot extends TelegramLongPollingBot {
         return text.substring(text.indexOf(" ")+1,text.length());
     }
 
-    private String processUserCommands(String command, User user, String text) {
+    private String processUserCommands(Parser.userCommands command, User user, String text) {
         String reply;
         switch(command) {
             case COMMAND_ADD:
@@ -108,7 +90,7 @@ public class RegBot extends TelegramLongPollingBot {
                 if(before == seekApproval.size()){
                     System.out.println("no change");
                 }
-                reply = COMMAND_ADDME;
+                reply = "ok";
                 System.out.println("addme");
                 break;
             case COMMAND_START:
@@ -121,7 +103,7 @@ public class RegBot extends TelegramLongPollingBot {
         return reply;
     }
 
-    private String processAdminCommands(String command, String text) {
+    private String processAdminCommands(Parser.adminCommands command, String text) {
         System.out.println("command is " + command);
         String reply = "";
         switch(command) {
@@ -133,24 +115,24 @@ public class RegBot extends TelegramLongPollingBot {
                 break;
             case ADMIN_PUBLISH:
                 System.out.println("admin publish");
-                reply = ADMIN_PUBLISH;
+                reply = "admin publish";
                 break;
             case ADMIN_REMOVE_PENDING:
                 System.out.println("admin remove pending");
-                reply = ADMIN_REMOVE_PENDING;
+                reply = "remove pending";
                 break;
             case ADMIN_REMOVE_USERS:
                 System.out.println("admin remove user");
                 AdminCommands.removeUser(myUsers, Integer.parseInt(text));
-                reply = ADMIN_REMOVE_USERS;
+                reply = "remove user";
                 break;
             case ADMIN_VIEW_REMIND:
                 System.out.println("admin remind user");
-                reply = ADMIN_VIEW_REMIND;
+                reply = "admin view remind";
                 break;
             case ADMIN_VIEW_REQUEST:
                 System.out.println("admin view request");
-                reply = ADMIN_VIEW_REQUEST;
+                reply = "request";
                 break;
             case ADMIN_VIEW_USERS:
                 System.out.println("admin view users");
@@ -165,7 +147,7 @@ public class RegBot extends TelegramLongPollingBot {
                 break;
             case ADMIN_HELP:
                 System.out.println("admin help");
-                reply = ADMIN_HELP;
+                reply = "admin help";
                 break;
             case ADMIN_NEWADMIN:
                 System.out.println("add new admin");
@@ -173,7 +155,7 @@ public class RegBot extends TelegramLongPollingBot {
                 reply = AdminCommands.getAdmins().toString();
                 break;
             default:
-                reply = "invalid";
+                reply = command.toString();
                 break;
         }
         return reply;
@@ -187,54 +169,8 @@ public class RegBot extends TelegramLongPollingBot {
         }
     }
 
-    private String getCommand(String text) {
-
-        if((text.length() > "/admin".length() && text.substring(0,"/admin".length()).equals("/admin"))){
-            if(text.length() >= ADMIN_ADD.length() && text.substring(0, ADMIN_ADD.length()).equals(ADMIN_ADD)) {
-                return ADMIN_ADD;
-            }else if(text.length() >= ADMIN_PUBLISH.length() && text.substring(0, ADMIN_PUBLISH.length()).equals(ADMIN_PUBLISH)){
-                return ADMIN_PUBLISH;
-            }else if(text.length() >= ADMIN_REMOVE_PENDING.length() && text.substring(0, ADMIN_REMOVE_PENDING.length()).equals(ADMIN_REMOVE_PENDING)){
-                return ADMIN_REMOVE_PENDING;
-            }else if(text.length() >= ADMIN_REMOVE_USERS.length() && text.substring(0, ADMIN_REMOVE_USERS.length()).equals(ADMIN_REMOVE_USERS)){
-                return ADMIN_REMOVE_USERS;
-            }else if(text.length() >= ADMIN_VIEW_PENDING.length() && text.substring(0, ADMIN_VIEW_PENDING.length()).equals(ADMIN_VIEW_PENDING)){
-                return ADMIN_VIEW_PENDING;
-            }else if(text.length() >= ADMIN_VIEW_REMIND.length() && text.substring(0, ADMIN_VIEW_REMIND.length()).equals(ADMIN_VIEW_REMIND)){
-                return ADMIN_VIEW_REMIND;
-            }else if(text.length() >= ADMIN_VIEW_REQUEST.length() && text.substring(0, ADMIN_VIEW_REQUEST.length()).equals(ADMIN_VIEW_REQUEST)){
-                return ADMIN_VIEW_REQUEST;
-            }else if(text.length() >= ADMIN_VIEW_USERS.length() && text.substring(0, ADMIN_VIEW_USERS.length()).equals(ADMIN_VIEW_USERS)){
-                return ADMIN_VIEW_USERS;
-            }else if(text.length() >= ADMIN_HELP.length() && text.substring(0, ADMIN_HELP.length()).equals(ADMIN_HELP)){
-                return ADMIN_HELP;
-            }else if(text.length() >= ADMIN_NEWADMIN.length() && text.substring(0, ADMIN_NEWADMIN.length()).equals(ADMIN_NEWADMIN)) {
-                return ADMIN_NEWADMIN;
-            }else{
-                return "invalid";
-            }
-        }else{
-            if(text.length() >= COMMAND_ADDME.length() && text.substring(0, COMMAND_ADDME.length()).equals(COMMAND_ADDME)){
-                return COMMAND_ADDME;
-            }else if(text.length() >= COMMAND_ADD.length() && text.substring(0, COMMAND_ADD.length()).equals(COMMAND_ADD)){
-                return COMMAND_ADD;
-            }else if(text.length() >= COMMAND_DELETE.length() && text.substring(0, COMMAND_DELETE.length()).equals(COMMAND_DELETE)){
-                return COMMAND_DELETE;
-            }else if(text.length() >= COMMAND_HELP.length() && text.substring(0, COMMAND_HELP.length()).equals(COMMAND_HELP)){
-                return COMMAND_HELP;
-            }else if(text.length() >= COMMAND_VIEW.length() && text.substring(0, COMMAND_VIEW.length()).equals(COMMAND_VIEW)){
-                return COMMAND_VIEW;
-            }else if(text.length() >= COMMAND_START.length() && text.substring(0, COMMAND_START.length()).equals(COMMAND_START)){
-                return COMMAND_START;
-            }else{
-                return "invalid";
-            }
-        }
-    }
-
     private String formatText(String input){
         String result = input.trim().replaceAll(" +", " ").replace("@ReggyBot ", "");
-
         return result;
     }
     private void sendReply(Message message, SendMessage sendMessage, String reply) {
