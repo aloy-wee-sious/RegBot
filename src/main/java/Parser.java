11c6b0d2a53507528891e790e219486aeeb5f79e
aloysius <aloy-wee-sious@hotmail.com>
2016-10-13 17:37:17 -0700

    added parser class

---
+++ src/main/java/Parser.java
@@ -0,0 +1,17 @@
+/**
+ * Created by aloysius on 10/13/16.
+ */
+public class Parser {
+
+    public enum userCommands{
+        COMMAND_ADD,
+        COMMAND_VIEW,
+        COMMAND_DELETE,
+        COMMAND_HELP,
+        COMMAND_ADDME
+    }
+
+
+
+
+}
